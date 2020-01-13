13ee4d8ff1153c43adef5db0cd00a4d7e3b7889a
Cristian Alvarez H <alvarezhernandezcristian1@gmail.com>
2020-01-12 20:13:57 -0500

    Create user data activity layout

---
+++ app/src/main/java/com/dev/cristian/alvarez/loginwithgoogleandfacebookdemo/UserDataActivity.kt
@@ -0,0 +1,18 @@
+package com.dev.cristian.alvarez.loginwithgoogleandfacebookdemo
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.ImageView
+import android.widget.TextView
+
+class UserDataActivity : AppCompatActivity() {
+
+    private val imgUser: ImageView? = null
+    private val txtNameUser: TextView? = null
+    private val txtEmailUser: TextView? = null
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_user_data)
+    }
+}
