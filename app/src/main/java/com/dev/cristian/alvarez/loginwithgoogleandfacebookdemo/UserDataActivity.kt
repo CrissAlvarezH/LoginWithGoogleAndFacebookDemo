1bfb70bdf8a9a9c86b9908ced105ed56c9ebf01a
Cristian Alvarez H <alvarezhernandezcristian1@gmail.com>
2020-01-14 22:38:27 -0500

    Get photo more large to Facebook provider

--- app/src/main/java/com/dev/cristian/alvarez/loginwithgoogleandfacebookdemo/UserDataActivity.kt
+++ app/src/main/java/com/dev/cristian/alvarez/loginwithgoogleandfacebookdemo/UserDataActivity.kt
@@ -6,6 +6,7 @@ import android.os.Bundle
 import android.view.View
 import android.widget.ImageView
 import android.widget.TextView
+import com.bumptech.glide.Glide
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.FirebaseUser
 
@@ -15,8 +16,9 @@ class UserDataActivity : AppCompatActivity() {
     private var txtNameUser: TextView? = null
     private var txtEmailUser: TextView? = null
     private var txtUidUser: TextView? = null
+    private var txtProvider: TextView? = null
 
-    private lateinit var authFire:  FirebaseAuth
+    private lateinit var authFire: FirebaseAuth
 
     override fun onStart() {
         super.onStart()
@@ -36,12 +38,29 @@ class UserDataActivity : AppCompatActivity() {
         txtUidUser = findViewById(R.id.txt_uid_user)
         txtEmailUser = findViewById(R.id.txt_user_email)
         txtNameUser = findViewById(R.id.txt_user_name)
+        txtProvider = findViewById(R.id.txt_provider)
     }
 
     fun setDataUI(user: FirebaseUser) {
+
+        Glide.with(this)
+            .load( getMoreLargePhotoUser(user) )
+            .into(imgUser!!)
+
         txtNameUser?.text = user.displayName
         txtEmailUser?.text = user.email
         txtUidUser?.text = user.uid
+        txtProvider?.text = user.providerId
+    }
+
+    private fun getMoreLargePhotoUser(user: FirebaseUser): String {
+        user.providerData.forEach { userInfo ->
+            if ( userInfo.providerId.contains("facebook.com") ) {
+                return "https://graph.facebook.com/" + userInfo.uid + "/picture?height=200";
+            }
+        }
+
+        return user.photoUrl.toString()
     }
 
     private fun toLogin() {
